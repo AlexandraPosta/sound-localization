1d461e112f7ba71d22f1f18aaa5027c263927be4
AlexandraPosta <posta_alex222@yahoo.com>
2023-01-05 13:40:30 +0200

    init commit

---
+++ Sound.m
@@ -0,0 +1,17 @@
+classdef Sound
+    %   Sound - Class used to define the properties of a given sound
+    %   Contains the name of the sound that is played; the data in vector
+    %   format and its location in X, Y, Z as float
+
+    properties
+        Name
+        Data
+        X
+        Y
+        Z
+    end
+
+    methods
+        % include method that plays sound
+    end
+end
